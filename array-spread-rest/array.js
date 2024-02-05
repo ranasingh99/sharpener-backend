19c04f7354b0befd5f00202c46e7f7c12127d924
ranasingh99 <sanketverma852@gmail.com>
2024-02-05 15:25:04 +0530

    REST Opeartors

--- array-spread-rest/array.js
+++ array-spread-rest/array.js
@@ -1,18 +1,33 @@
 // SPREAD OPERATORS
 
-function sumoftwo(a,b){
-    return a+b;
-}
+// function sumoftwo(a,b){
+//     return a+b;
+// }
 
-let myNum = [20,25];
+// let myNum = [20,25];
 
-console.log(sumoftwo(...myNum));
+// console.log(sumoftwo(...myNum));
 
-/* so rest opeartors taken into account when we do not wish to change the function 
+/* so spread opeartors taken into account when we do not wish to change the function 
  defination of our function ...so in the above example the function is expexting the 
  individul value but we are passing it the array object , so in this situation the 
 
  rest operators comes into the picture
- REST OPeartors : Basically rest opeartors takes group and spread it into individual 
+ SPREAD OPeartors : Basically rest opeartors takes group and spread it into individual 
  value.
  */
+
+ // REST OPearatos : when we are not sure about the number of argumets that can be 
+ // passed that time we use rest operatos; for example;
+
+
+function sumOfNumber(...args){
+    let sum = 0;
+    for(let arg of args){
+        sum += arg;
+    }
+    return sum;
+}
+
+console.log(sumOfNumber(3,4));
+// here args is variable and it combines all arguments value and bind it in array;
