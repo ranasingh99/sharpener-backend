9c97112e59314d5436056c10095bf5e2a3a8f936
ranasingh99 <sanketverma852@gmail.com>
2024-02-05 15:12:56 +0530

    REST Opeartors

--- array-spread-rest/array.js
+++ array-spread-rest/array.js
@@ -1,19 +1,18 @@
-const fruits = ['Apple','orange','','mango','','lemon'];
+// SPREAD OPERATORS
 
-// const myNums = [1,2,3,4,5,6,7,,8,9,10];
+function sumoftwo(a,b){
+    return a+b;
+}
 
-// const newNums = myNums.filter((num)=>num>4);
+let myNum = [20,25];
 
-// console.log(newNums);
- 
+console.log(sumoftwo(...myNum));
 
-const newFruits = fruits.map((fruit)=>{
-    if(fruit === ''){
-        return ('empty string'+fruit);
-    }else return fruit;
-})
+/* so rest opeartors taken into account when we do not wish to change the function 
+ defination of our function ...so in the above example the function is expexting the 
+ individul value but we are passing it the array object , so in this situation the 
 
-// note when we use curly braces then it becomes necesssary to use the return 
-// ketword inside the scope;
-
-console.log(newFruits);
+ rest operators comes into the picture
+ REST OPeartors : Basically rest opeartors takes group and spread it into individual 
+ value.
+ */
