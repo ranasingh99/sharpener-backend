8809ff969ca81f857931fba6d2dc579ba763e791
ranasingh99 <sanketverma852@gmail.com>
2024-02-07 18:27:19 +0530

    redirecting the page to the '/ 'path

---
+++ server/basicfile.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+      
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="msg"><button type="submit">SEND</button></form></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+    if(url === '/message' && method ==='POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+})
+
+server.listen(3000);
